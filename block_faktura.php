a7098b86947c77c28a9832b5e6189924c80b0dd4
runtyrobot <mail@runtyrobot.com>
2016-06-22 13:49:31 +0200

    Initial Commit

---
+++ block_faktura.php
@@ -0,0 +1,14 @@
+<?php
+
+/*------------------------------------------------------------
+    Innowell Faktura-block for Moodle
+    by Theis Thompson - theisthompson@gmail.com
+------------------------------------------------------------*/
+
+class block_faktura extends block_base {
+
+    public function init() {
+        $this->title = get_string();
+    }
+
+}
